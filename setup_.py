68b34b9c74330aeba8a522d88749e46cdd10b8a9
jahanxb <jahanxbkhan@yandex.com>
2019-10-11 19:05:00 +0800

    Add files via upload
    I forgot to Fork it :P

---
+++ setup_.py
@@ -0,0 +1,20 @@
+
+from post_list import PostList
+
+POST_HOUR = 1
+POST_DAYS = ['sunday','monday', 'tuesday','friday','saturday','sunday','thursday', 'wednesday']
+POST_LIST = PostList([])
+DATES_OF_POSTS = {}
+
+USERNAME = "dumblaowai"
+PASSWORD = "Nfcietmultan1"
+
+DAYS_MAP = {
+    0: 'monday',
+    1: 'tuesday',
+    2: 'wednesday',
+    3: 'thursday',
+    4: 'friday',
+    5: 'saturday',
+    6 :'sunday',
+}
